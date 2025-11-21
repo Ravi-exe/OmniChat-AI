095a142f0f9fe41a0dd5192be8283cf63635d15b
Ravi-exe <ravi.software@outlook.com>
2025-11-21 16:07:30 +0530

    changed structure and it will also change in next commit becuase ik this is also not perfect

---
+++ frontend/src/components/prompt/prompt.component.tsx
@@ -0,0 +1,23 @@
+
+
+
+export default function PromptForm() {
+
+    return  (
+        <div className="prompt-container w-100">
+            <form className="prompt-form">
+                <div className="form-control">
+                    <label htmlFor="prompt-input">Try it !!!</label>
+                    <input type="text" id="prompt-input" placeholder="Type me and kill me" />
+                </div>
+
+                <input type="">
+                </input>
+                // <select>
+
+                // </select>
+
+            </form>
+        </div>
+    )
+}
---
+++ frontend/src/components/sidebar/sidebar.component.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Sidebar() {
+
+
+    return (
+            <section className="w-40 h-100">
+                
+            </section>
+        )
+}
